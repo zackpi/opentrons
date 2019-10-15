efbc3beb223eef0b5066b9934a09061fa472ceb2
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2019-10-15 16:43:59 -0400

    add print as argument

--- api/src/opentrons/drivers/log_parser.py
+++ api/src/opentrons/drivers/log_parser.py
@@ -1,5 +1,6 @@
 import argparse
 import difflib
+import json
 import re
 import sys
 
@@ -15,16 +16,18 @@ parser_args = {
     }
 
 
-def get_str_list(log_file, substrings: list):
-    content_str = log_file.read().decode('utf-8')
+def strip_unnecessary_char(line: str):
+    return line.strip().strip("'").strip("\\r\\n\\r\\n")
+
+
+def get_str_list(log_file: TextIO, substrings: list):
+    content_str = log_file.readlines()
     filtered_contents = list(
         filter(lambda line: all(substr in line for substr in substrings),
                content_str))
-    print(filtered_contents)
     for line in filtered_contents:
-        new_line = line.split('smoothie: Write -> b')[1].rstrip()
-        formatted_line = new_line.strip("'").strip('\\r\\n\\r\\n')
-        print(formatted_line)
+        new_line = line.split('smoothie: Write -> b')[1]
+        formatted_line = strip_unnecessary_char(new_line)+'\n'
         yield interpret_gcode(formatted_line)
 
 
@@ -38,11 +41,11 @@ def interpret_gcode(line):
 def compare(log_1: TextIO,
             log_2: TextIO,
             log_type: str):
-    # print(log_1)
     contents_1 = list(get_str_list(log_1, parser_args[log_type]))
     contents_2 = list(get_str_list(log_2, parser_args[log_type]))
-    print(contents_1)
-    print(contents_2)
+    result = difflib.ndiff(contents_1, contents_2)
+    return result
+    # sys.stdout.writelines()
     # output_file = open(f"{log_type}.html", "w")
     # hd = difflib.HtmlDiff()
     # output_file.write(
@@ -53,15 +56,18 @@ def compare(log_1: TextIO,
 def get_arguments(
         parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser.add_argument(
-        'log_1', type=argparse.FileType('rb'),
-        help='The first log file to compare')
-    parser.add_argument(
-        'log_2', type=argparse.FileType('rb'),
-        help='The second log file to compare')
-    parser.add_argument(
         'log_type', action='store',
         choices=['serial', 'api'],
         help='Type of logs')
+    parser.add_argument(
+        'log_1', type=argparse.FileType('r', encoding='UTF-8'),
+        help='The first log file to compare')
+    parser.add_argument(
+        'log_2', type=argparse.FileType('r', encoding='UTF-8'),
+        help='The second log file to compare')
+    parser.add_argument(
+        '-p', '--print', action='store_true', default=False,
+        help='Print the differences in command line')
     return parser
 
 
@@ -71,9 +77,12 @@ def main() -> int:
     parser = get_arguments(parser)
     args = parser.parse_args()
 
-    compare(args.log_1,
-            args.log_2,
-            args.log_type)
+    result = compare(args.log_1,
+                     args.log_2,
+                     args.log_type)
+
+    if args.print:
+        print("".join(result))
 
     return 0
 
